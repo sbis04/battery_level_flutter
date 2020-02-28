78ae4e9660a8f46ba7ad2d669fba799f5b41ebd7
Souvik Biswas <sbis1999@gmail.com>
2020-02-28 06:58:50 +0530

    Added some comments

--- lib/battery_page.dart
+++ lib/battery_page.dart
@@ -1,6 +1,9 @@
 import 'package:battery_level_pc/util.dart';
 import 'package:flutter/material.dart';
 
+// UI for displaying the battery
+// information retreived from the
+// native android part.
 class BatteryPage extends StatefulWidget {
   @override
   _BatteryPageState createState() => _BatteryPageState();
@@ -21,8 +24,10 @@ class _BatteryPageState extends State<BatteryPage> {
         color: Colors.orange[100],
         child: Center(
           child: FutureBuilder(
+            // Using a future object for handling async call
             future: batteryLevel.getBatteryLevel(),
             builder: (context, snapshot) {
+              // Capturing the data from the snapshot
               batteryLevelString = snapshot.data;
               return Text(
                 batteryLevelString,
--- lib/util.dart
+++ lib/util.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/services.dart';
 
+// Helper class for fetching the battery level
 class FetchBatteryLevel {
+  // Setting up the platform channel specifying
+  // a name for communication
   static const platform = const MethodChannel("com.souvikbiswas.demo/battery");
 
+  // Function for calling a method
+  // present in the native android
+  // app code
   Future<String> getBatteryLevel() async {
     String batteryLevelString = '';
     try {
+      // Retrieving the result using the native method
       final int result = await platform.invokeMethod('getBatteryLevel');
 
       batteryLevelString = "Battery Level is : $result";
